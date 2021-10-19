5dc670b36a13b0ae2db5d47d1975f6c850779414
Daniel App <32045098+danbapp@users.noreply.github.com>
2021-10-19 10:49:51 -0400

    Assignment Completed

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
 const cats = ["Milo", "Otis", "Garfield"];
 
-function destructivelyAppendCat(name) {
-  cats.push('Ralph');
+function destructivelyAppendCat(name){
+  return cats.push(name);
 }
 
-function destructivelyPrependCat(name) {
-  cats.unshift('Bob');
+function destructivelyPrependCat(name){
+  return cats.unshift(name);
 }
 
-function destructivelyRemoveLastCat(name) {
-  cats.pop();
+function destructivelyRemoveLastCat(name){
+  return cats.pop(name);
 }
 
-function destructivelyRemoveFirstCat(name) {
-  cats.shift();
+function destructivelyRemoveFirstCat(name){
+  return cats.shift(name);
+}
+
+function appendCat(name){
+  const append = [...cats, 'Broom'];
+  return append;
+}
+
+function prependCat(name){
+  const append = ['Arnold', ...cats];
+  return append;
+}
+
+function removeLastCat(){
+  const removeLast = cats.slice(0, 2);
+  return removeLast;
+}
+
+function removeFirstCat(){
+  const removeFirst = cats.slice(1, 3);
+  return removeFirst;
 }
